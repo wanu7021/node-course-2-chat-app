994c5469dcbc9ad9916b26b487d0c9b3c52c65d8
Manuel Iglesias <miglesias@salesforce.com>
2018-02-14 13:34:43 -0800

    Add form for messages and show incoming messages from browser

--- public/js/index.js
+++ public/js/index.js
@@ -6,8 +6,23 @@ socket.on('connect', () => {
 
 socket.on('newMessage', (message) => {
   console.log('New Message', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
 });
 
 socket.on('disconnect', () => {
   console.log('Disconnected from server')
+});
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, () => {
+
+  });
 });
