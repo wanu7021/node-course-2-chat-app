68e3d4c18afd6c018dc2830d5a709e0502ca5cda
Manuel Iglesias <miglesias@salesforce.com>
2018-02-15 09:44:10 -0800

    Added mustache for message template

--- public/js/index.js
+++ public/js/index.js
@@ -6,19 +6,24 @@ socket.on('connect', () => {
 
 socket.on('newMessage', (message) => {
   const formattedTime = moment(message.createdAt).format('h:mm a');
-  let li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  jQuery('#messages').append(li);
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    from: message.from,
+    createdAt: formattedTime
+  });
+  jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', (message) => {
   const formattedTime = moment(message.createdAt).format('h:mm a');
-  let li = jQuery('<li></li>');
-  let a = jQuery('<a target="_blank">My current location</a>');
-  li.text(`${message.from} ${formattedTime}: `);
-  a.attr('href', message.url);
-  li.append(a);
-  jQuery('#messages').append(li);
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    url: message.url,
+    from: message.from,
+    createdAt: formattedTime
+  }); 
+  jQuery('#messages').append(html);
 });
 
 socket.on('disconnect', () => {
